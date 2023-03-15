55a8ac27184d8655d2415788a470e35bff77d5bf
N1k1ta-blip <niklis60003@gmail.com>
2023-03-15 12:14:35 +0300

    clean imports

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState, useRef } from 'react';
+import { Suspense, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 
 import { Points, PointMaterial, Preload } from '@react-three/drei';
